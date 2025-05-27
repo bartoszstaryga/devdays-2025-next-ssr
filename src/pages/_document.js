04b6dd285e0e38a95a6641ca6f5accc342b30fc9
Bartosz Staryga <bartosz.staryga@magnolia-cms.com>
2025-05-27 14:33:35 +0200

    Add GSC

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <meta name='google-site-verification' content='Opc-WIa4rGXZ19Z8nC1rBPP1ipuEcac77tdOe1foD6U' />
+      </Head>
       <body>
         <Main />
         <NextScript />
